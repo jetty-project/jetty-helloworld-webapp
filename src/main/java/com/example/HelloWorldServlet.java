a5c6893feba53795cb53fc52ea37d01ab03ac5fd
Joakim Erdfelt <joakim.erdfelt@gmail.com>
2020-11-09 06:37:42 -0600

    Fixing build for Java 11 and licenses

--- src/main/java/com/example/HelloWorldServlet.java
+++ src/main/java/com/example/HelloWorldServlet.java
@@ -1,10 +1,28 @@
+//
+//  ========================================================================
+//  Copyright (c) Mort Bay Consulting Pty Ltd and others.
+//  ------------------------------------------------------------------------
+//  All rights reserved. This program and the accompanying materials
+//  are made available under the terms of the Eclipse Public License v1.0
+//  and Apache License v2.0 which accompanies this distribution.
+//
+//      The Eclipse Public License is available at
+//      http://www.eclipse.org/legal/epl-v10.html
+//
+//      The Apache License v2.0 is available at
+//      http://www.opensource.org/licenses/apache2.0.php
+//
+//  You may elect to redistribute this code under either of these licenses.
+//  ========================================================================
+//
+
 package com.example;
 
+import java.io.IOException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 @WebServlet(urlPatterns = {"/*"}, loadOnStartup = 1)
 public class HelloWorldServlet extends HttpServlet 
